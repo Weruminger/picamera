79d4cab0adac494f0d45f8c49ef3c36f3d65dd78
Rolf Werum <Weruminger@googlemail.com>
2020-05-22 21:18:11 +0200

    Cam test wit image

--- samples/TestBlankLenseShiftArrays.py
+++ samples/TestBlankLenseShiftArrays.py
@@ -1,6 +1,6 @@
 from picamera import PiCamera
 import numpy as np
-import time
+import time, datatime
 
 with PiCamera() as cam:
     lst_shape = cam._lens_shading_table_shape()
@@ -9,6 +9,9 @@ lst = np.zeros(lst_shape, dtype=np.uint8)
 lst[...] = 32 # NB 32 corresponds to unity gain
 
 with PiCamera(lens_shading_table=lst) as cam:
-    cam.start_preview()
-    time.sleep(5)
-    cam.stop_preview()
+    # cam.start_preview()
+    # time.sleep(5)
+    # cam.stop_preview()
+    cam.resolution = (1280,720)
+    cam.capture("./newimage" + datatime.now().replace("/","_").replace(".","_").replace(":","_").replace(" ","_")  + ".jpg")
+
